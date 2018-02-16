748fdbfe636dfb700e7781a73b35587793c486cf
Nick Gravelyn <nick@gravelyn.com>
2018-02-15 20:32:17 -0800

    Actually getting something animating

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,53 @@
-import pkg from './package.json'
+import buble from 'rollup-plugin-buble'
+import uglify from 'rollup-plugin-uglify'
 
 export default [
   {
     input: 'src/index.js',
-    output: {
-      file: pkg.browser,
-      format: 'umd',
-      name: 'cssAnimationSequencer'
-    },
-    plugins: []
+    output: [
+      {
+        file: 'dist/css-animation-sequencer.cjs.js',
+        format: 'cjs'
+      },
+      {
+        file: 'dist/css-animation-sequencer.esm.js',
+        format: 'es'
+      },
+      {
+        file: 'dist/css-animation-sequencer.umd.js',
+        format: 'umd',
+        name: 'CssAnimationSequencer'
+      },
+      {
+        file: 'dist/css-animation-sequencer.iife.js',
+        format: 'iife',
+        name: 'CssAnimationSequencer'
+      }
+    ],
+    plugins: [buble()]
   },
   {
     input: 'src/index.js',
     output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' }
+      {
+        file: 'dist/css-animation-sequencer.cjs.min.js',
+        format: 'cjs'
+      },
+      {
+        file: 'dist/css-animation-sequencer.esm.min.js',
+        format: 'es'
+      },
+      {
+        file: 'dist/css-animation-sequencer.umd.min.js',
+        format: 'umd',
+        name: 'CssAnimationSequencer'
+      },
+      {
+        file: 'dist/css-animation-sequencer.iife.min.js',
+        format: 'iife',
+        name: 'CssAnimationSequencer'
+      }
     ],
-    plugins: []
+    plugins: [buble(), uglify()]
   }
 ]
--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('hello, world!')
+export default class CssAnimationSequencer {
+  constructor () {
+    this._animations = []
+  }
+
+  add (element, className) {
+    this._animations.push({ element, className })
+  }
+
+  play () {
+    this._doNextAnimation()
+  }
+
+  _doNextAnimation () {
+    if (this._animations.length === 0) return
+
+    const { element, className } = this._animations.shift()
+
+    element.classList.add(className)
+
+    const listener = () => {
+      element.classList.remove(className)
+      this._doNextAnimation()
+    }
+    element.addEventListener('animationend', listener)
+  }
+}
