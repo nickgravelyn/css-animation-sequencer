4e5ca7623b98e5e4039e630833494c14b775ec3b
Nick Gravelyn <nick@gravelyn.com>
2018-02-15 21:37:55 -0800

    Improving the same and code a bit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
+class Animation {
+  constructor (element, className, sequencer) {
+    this._element = element
+    this._className = className
+
+    this._listener = () => {
+      this.stop()
+      sequencer._doNextAnimation()
+    }
+  }
+
+  start () {
+    this._element.classList.add(this._className)
+    this._element.addEventListener('animationend', this._listener)
+  }
+
+  stop () {
+    this._element.classList.remove(this._className)
+    this._element.removeEventListener('animationend', this._listener)
+  }
+}
+
 export default class CssAnimationSequencer {
   constructor () {
     this._animations = []
   }
 
   add (element, className) {
-    this._animations.push({ element, className })
+    this._animations.push(new Animation(element, className, this))
   }
 
   play (args = {}) {
@@ -13,25 +35,19 @@ export default class CssAnimationSequencer {
     this._doNextAnimation()
   }
 
+  stop () {
+    const current = (this._next - 1) % this._animations.length
+    this._animations[current].stop()
+  }
+
   _doNextAnimation () {
     if (this._animations.length === this._next) {
-      if (this._loops == 0)
-        return
-      if (this._loops > 0)
-        this._loops--
+      if (this._loops == 0) return
+      if (this._loops > 0) this._loops--
 
       this._next = 0
     }
 
-    const { element, className } = this._animations[this._next++]
-
-    element.classList.add(className)
-
-    const listener = () => {
-      element.classList.remove(className)
-      element.removeEventListener('animationend', listener)
-      this._doNextAnimation()
-    }
-    element.addEventListener('animationend', listener)
+    this._animations[this._next++].start()
   }
 }
