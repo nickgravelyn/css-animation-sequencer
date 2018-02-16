a614a1020218ee27c905c4f567b3575cbc977ad3
Nick Gravelyn <nick@gravelyn.com>
2018-02-15 21:09:52 -0800

    Got looping in place

--- src/index.js
+++ src/index.js
@@ -7,19 +7,29 @@ export default class CssAnimationSequencer {
     this._animations.push({ element, className })
   }
 
-  play () {
+  play (args = {}) {
+    this._loops = args.loops || 0
+    this._next = 0
     this._doNextAnimation()
   }
 
   _doNextAnimation () {
-    if (this._animations.length === 0) return
+    if (this._animations.length === this._next) {
+      if (this._loops == 0)
+        return
+      if (this._loops > 0)
+        this._loops--
 
-    const { element, className } = this._animations.shift()
+      this._next = 0
+    }
+
+    const { element, className } = this._animations[this._next++]
 
     element.classList.add(className)
 
     const listener = () => {
       element.classList.remove(className)
+      element.removeEventListener('animationend', listener)
       this._doNextAnimation()
     }
     element.addEventListener('animationend', listener)
