8a611280a3ac5d182ccdef8ac27b10be573c1d68
Nick Gravelyn <nick@gravelyn.com>
2018-02-21 12:26:54 -0800

    Loosening Babel compiling so we get simpler prototype code generation since inheritance isn't used anywhere in the code

--- dist/seashell.esm.js
+++ dist/seashell.esm.js
@@ -12,24 +12,6 @@ var classCallCheck = function (instance, Constructor) {
   }
 };
 
-var createClass = function () {
-  function defineProperties(target, props) {
-    for (var i = 0; i < props.length; i++) {
-      var descriptor = props[i];
-      descriptor.enumerable = descriptor.enumerable || false;
-      descriptor.configurable = true;
-      if ("value" in descriptor) descriptor.writable = true;
-      Object.defineProperty(target, descriptor.key, descriptor);
-    }
-  }
-
-  return function (Constructor, protoProps, staticProps) {
-    if (protoProps) defineProperties(Constructor.prototype, protoProps);
-    if (staticProps) defineProperties(Constructor, staticProps);
-    return Constructor;
-  };
-}();
-
 var SetStep = function () {
   function SetStep(element, state) {
     classCallCheck(this, SetStep);
@@ -38,19 +20,15 @@ var SetStep = function () {
     this._state = state;
   }
 
-  createClass(SetStep, [{
-    key: 'start',
-    value: function start(next) {
-      applyState(this._element, this._state);
-      next();
-    }
-  }, {
-    key: 'stop',
-    value: function stop() {}
-  }, {
-    key: 'createCss',
-    value: function createCss() {}
-  }]);
+  SetStep.prototype.start = function start(next) {
+    applyState(this._element, this._state);
+    next();
+  };
+
+  SetStep.prototype.stop = function stop() {};
+
+  SetStep.prototype.createCss = function createCss() {};
+
   return SetStep;
 }();
 
@@ -68,41 +46,37 @@ var ToStep = function () {
     this._animation = 'seashell-' + randomInt();
   }
 
-  createClass(ToStep, [{
-    key: 'start',
-    value: function start(next) {
-      var _this = this;
+  ToStep.prototype.start = function start(next) {
+    var _this = this;
 
-      this._listener = function () {
-        _this.stop();
-        applyState(_this._element, _this._state);
-        next();
-      };
+    this._listener = function () {
+      _this.stop();
+      applyState(_this._element, _this._state);
+      next();
+    };
 
-      this._element.addEventListener('animationend', this._listener);
-      this._element.classList.add(this._animation);
-    }
-  }, {
-    key: 'stop',
-    value: function stop() {
-      this._element.removeEventListener('animationend', this._listener);
-      this._element.classList.remove(this._animation);
-    }
-  }, {
-    key: 'createCss',
-    value: function createCss(timelineId) {
-      var css = '.' + this._animation + ' { animation: ' + this._animation + ' ' + this._duration / 1000 + 's both; } ';
-      css += '@keyframes ' + this._animation + ' { to { ';
+    this._element.addEventListener('animationend', this._listener);
+    this._element.classList.add(this._animation);
+  };
 
-      for (var key in this._state) {
-        if (this._state.hasOwnProperty(key)) {
-          css += key + ': ' + this._state[key] + '; ';
-        }
+  ToStep.prototype.stop = function stop() {
+    this._element.removeEventListener('animationend', this._listener);
+    this._element.classList.remove(this._animation);
+  };
+
+  ToStep.prototype.createCss = function createCss(timelineId) {
+    var css = '.' + this._animation + ' { animation: ' + this._animation + ' ' + this._duration / 1000 + 's both; } ';
+    css += '@keyframes ' + this._animation + ' { to { ';
+
+    for (var key in this._state) {
+      if (this._state.hasOwnProperty(key)) {
+        css += key + ': ' + this._state[key] + '; ';
       }
-
-      return css + '} }';
     }
-  }]);
+
+    return css + '} }';
+  };
+
   return ToStep;
 }();
 
@@ -113,21 +87,17 @@ var RunStep = function () {
     this._timeline = timeline;
   }
 
-  createClass(RunStep, [{
-    key: "start",
-    value: function start(next) {
-      this._timeline.play();
-      next();
-    }
-  }, {
-    key: "stop",
-    value: function stop() {
-      this._timeline.stop();
-    }
-  }, {
-    key: "createCss",
-    value: function createCss() {}
-  }]);
+  RunStep.prototype.start = function start(next) {
+    this._timeline.play();
+    next();
+  };
+
+  RunStep.prototype.stop = function stop() {
+    this._timeline.stop();
+  };
+
+  RunStep.prototype.createCss = function createCss() {};
+
   return RunStep;
 }();
 
@@ -146,86 +116,76 @@ var Timeline = function () {
     };
   }
 
-  createClass(Timeline, [{
-    key: 'set',
-    value: function set$$1(element, state) {
-      this._throwIfBaked();
-      this._steps.push(new SetStep(element, state));
-    }
-  }, {
-    key: 'to',
-    value: function to(element, duration, state) {
-      this._throwIfBaked();
-      this._steps.push(new ToStep(element, duration, state));
-    }
-  }, {
-    key: 'run',
-    value: function run(timeline) {
-      this._throwIfBaked();
-      this._steps.push(new RunStep(timeline));
-    }
-  }, {
-    key: 'play',
-    value: function play() {
-      var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+  Timeline.prototype.set = function set$$1(element, state) {
+    this._throwIfBaked();
+    this._steps.push(new SetStep(element, state));
+  };
 
-      if (!this._baked) this._bake();
+  Timeline.prototype.to = function to(element, duration, state) {
+    this._throwIfBaked();
+    this._steps.push(new ToStep(element, duration, state));
+  };
 
-      this._iterations = options.iterations || 1;
+  Timeline.prototype.run = function run(timeline) {
+    this._throwIfBaked();
+    this._steps.push(new RunStep(timeline));
+  };
+
+  Timeline.prototype.play = function play() {
+    var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+    if (!this._baked) this._bake();
+
+    this._iterations = options.iterations || 1;
+    this._stepIndex = 0;
+    this._runStep();
+  };
+
+  Timeline.prototype.stop = function stop() {
+    this._steps[this._stepIndex].stop();
+    this._styleElement.parentNode.removeChild(this._styleElement);
+    this._styleElement = null;
+    this._baked = false;
+  };
+
+  Timeline.prototype._throwIfBaked = function _throwIfBaked() {
+    if (this._baked) throw new Error('Cannot add to a Timeline after play is called');
+  };
+
+  Timeline.prototype._runStep = function _runStep() {
+    if (this._iterations === 0) {
+      this.stop();
+      return;
+    }
+
+    this._steps[this._stepIndex].start(this._nextExecute);
+  };
+
+  Timeline.prototype._toNextStep = function _toNextStep() {
+    this._stepIndex++;
+    if (this._steps.length <= this._stepIndex) {
       this._stepIndex = 0;
-      this._runStep();
-    }
-  }, {
-    key: 'stop',
-    value: function stop() {
-      this._steps[this._stepIndex].stop();
-      this._styleElement.parentNode.removeChild(this._styleElement);
-      this._styleElement = null;
-      this._baked = false;
-    }
-  }, {
-    key: '_throwIfBaked',
-    value: function _throwIfBaked() {
-      if (this._baked) throw new Error('Cannot add to a Timeline after play is called');
-    }
-  }, {
-    key: '_runStep',
-    value: function _runStep() {
-      if (this._iterations === 0) {
-        this.stop();
-        return;
-      }
 
-      this._steps[this._stepIndex].start(this._nextExecute);
+      if (this._iterations === Infinity) return;
+      if (this._iterations > 0) this._iterations--;
     }
-  }, {
-    key: '_toNextStep',
-    value: function _toNextStep() {
-      this._stepIndex++;
-      if (this._steps.length <= this._stepIndex) {
-        this._stepIndex = 0;
+  };
 
-        if (this._iterations === Infinity) return;
-        if (this._iterations > 0) this._iterations--;
+  Timeline.prototype._bake = function _bake() {
+    if (this._baked) return;
+
+    this._styleElement = document.createElement('style');
+    for (var i = 0; i < this._steps.length; ++i) {
+      var css = this._steps[i].createCss();
+      if (css) {
+        this._styleElement.textContent += css + '\n';
       }
     }
-  }, {
-    key: '_bake',
-    value: function _bake() {
-      if (this._baked) return;
+    document.head.appendChild(this._styleElement);
 
-      this._styleElement = document.createElement('style');
-      for (var i = 0; i < this._steps.length; ++i) {
-        var css = this._steps[i].createCss();
-        if (css) {
-          this._styleElement.textContent += css + '\n';
-        }
-      }
-      document.head.appendChild(this._styleElement);
+    this._baked = true;
+  };
 
-      this._baked = true;
-    }
-  }]);
   return Timeline;
 }();
 
