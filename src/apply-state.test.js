aadb74e1b72d9a8f68c77e9e05d5496a4822a0e5
Nick Gravelyn <nick@gravelyn.com>
2018-02-21 20:31:50 -0800

    improving test a little bit

--- src/apply-state.test.js
+++ src/apply-state.test.js
@@ -1,7 +1,7 @@
 import applyState from './apply-state'
 
 it('copies key/values from the state onto the style block of the element', () => {
-  const element = { style: { c: true } }
+  const element = { style: { b: 'dog', c: true } }
   const state = { a: 5, b: 'cat' }
   applyState(element, state)
 
