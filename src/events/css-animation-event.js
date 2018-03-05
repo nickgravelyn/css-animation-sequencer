c20b8e737f9164863c17bee5d99373091b8864d4
Nick Gravelyn <nick@gravelyn.com>
2018-03-04 22:04:19 -0800

    add event to play css animation

---
+++ src/events/css-animation-event.js
@@ -0,0 +1,21 @@
+export class CssAnimationEvent {
+  constructor (element, className) {
+    this.element = element
+    this.animation = className
+  }
+
+  start (next) {
+    this.listener = () => {
+      this.stop()
+      next()
+    }
+
+    this.element.addEventListener('animationend', this.listener)
+    this.element.classList.add(this.animation)
+  }
+
+  stop () {
+    this.element.removeEventListener('animationend', this.listener)
+    this.element.classList.remove(this.animation)
+  }
+}
---
+++ src/events/css-animation-event.test.js
@@ -0,0 +1,55 @@
+import { CssAnimationEvent } from './css-animation-event'
+
+function makeFakeElem () {
+  return {
+    addEventListener (event, fn) {
+      this.event = event
+      this.fn = fn
+    },
+    removeEventListener: jest.fn(),
+    classList: {
+      add: jest.fn(),
+      remove: jest.fn(),
+    },
+  }
+}
+
+it('adds event listener and class on start', () => {
+  const elem = makeFakeElem()
+  const step = new CssAnimationEvent(elem, 'anim-class')
+  step.start(jest.fn())
+
+  expect(elem.event).toBe('animationend')
+  expect(elem.fn).toBeDefined()
+  expect(elem.classList.add).toHaveBeenCalledWith('anim-class')
+})
+
+it('creates a listener that removes class and listener and calls next', () => {
+  const elem = makeFakeElem()
+  const step = new CssAnimationEvent(elem, 'anim-class')
+  const next = jest.fn()
+
+  step.start(next)
+
+  expect(next).not.toHaveBeenCalled()
+  expect(elem.classList.remove).not.toHaveBeenCalled()
+  expect(elem.removeEventListener).not.toHaveBeenCalled()
+
+  // Invoke our animationend listener
+  elem.fn()
+
+  expect(next).toHaveBeenCalled()
+  expect(elem.classList.remove).toHaveBeenCalledWith('anim-class')
+  expect(elem.removeEventListener).toHaveBeenCalledWith('animationend', elem.fn)
+})
+
+it('removes event listener and class on stop', () => {
+  const elem = makeFakeElem()
+  const step = new CssAnimationEvent(elem, 'anim-class')
+
+  step.start(jest.fn())
+  step.stop()
+
+  expect(elem.removeEventListener).toHaveBeenCalledWith('animationend', elem.fn)
+  expect(elem.classList.remove).toHaveBeenCalledWith('anim-class')
+})
