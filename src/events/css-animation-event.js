7552eb1ca19daf95707fa8c2a08e0ed33d85cff1
Nick Gravelyn <nick@gravelyn.com>
2018-03-05 21:02:10 -0800

    Optimizing css animation event to reduce GC pressure

--- src/events/css-animation-event.js
+++ src/events/css-animation-event.js
@@ -2,20 +2,22 @@ export class CssAnimationEvent {
   constructor (element, className) {
     this._element = element
     this._animation = className
+    this._onAnimationEnd = this._onAnimationEnd.bind(this)
   }
 
   start (complete) {
-    this.listener = () => {
-      this.stop()
-      complete()
-    }
-
-    this._element.addEventListener('animationend', this.listener)
+    this._complete = complete
+    this._element.addEventListener('animationend', this._onAnimationEnd)
     this._element.classList.add(this._animation)
   }
 
   stop () {
-    this._element.removeEventListener('animationend', this.listener)
+    this._element.removeEventListener('animationend', this._onAnimationEnd)
     this._element.classList.remove(this._animation)
+  }
+
+  _onAnimationEnd () {
+    this.stop()
+    this._complete()
   }
 }
